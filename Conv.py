237f84fb308104a870f87b76a6c6bb63251a9cc6
Babou426 <diopabubakr79@gmail.com>
2022-04-06 06:43:20 +0000

    Ajout

--- Conv.py
+++ Conv.py
@@ -2,75 +2,52 @@
 import xml.etree.ElementTree as T
 from sqlalchemy import JSON
 from csv import DictReader as csv_load
-from csv import DictWriter as csv_write
-import yaml
+from yaml import safe_load
 import xmltodict
 from pathlib import Path
 from ruamel.yaml import YAML
+from yaml import dump 
 from dict2xml import dict2xml
 from convertir import *
-from dict import *
-import csv
 import json
 from pathlib import Path
 import xmltodict
 from ruamel.yaml import YAML
 #########################################################
-file = """
-    {
-        "employees":
-        [
-            { 
-              "firstName":"John",
-              "lastName":"Doe" 
-            },
-            { 
-              "firstName":"Anna", 
-              "lastName":"Smith" 
-            },
-            { 
-              "firstName":"Peter", 
-              "lastName":"Jones" 
-            }
-        ]
-    }
-"""
+
+
+
 ########################## YAML #####################
 def yaml_to_dict(file):
+
     c = Path(file)
     yaml = YAML(typ="safe")
     f =c.open("r", encoding="utf8")
     d = yaml.load(f)
     return d
-#print(yaml_dict(file))
+# print(yaml_to_dict('yaml.yaml')) OK
 
 ######################### CSV #######################
-import csv
-file = 'doc.csv'
+
 
 def csv_to_dict(file):
-    with open('file.csv', 'w') as f:   
-        data = [data]
-        fieldnames = data[0].keys()
-        output = csv_write(f, fieldnames)
-        output.writeheader()
-        for elem in data:
-            output.writerow(elem)
+    with open(file, 'r') as f:
+        liste = []
 
+        r_csv = csv_load(f)
+        for i in r_csv:
+            liste.append(i)
+        return liste
 
 ######################### XML ##########################
-# file = 'doc.xml'
+
 def xml_to_dict(file):
-    with open(file) as f:
+    with open(file,'r') as f:
         doc = xmltodict.parse(f.read())
         return json.loads(json.dumps(doc))
 
-# print(xml_to_dict((file)))
-
 ########################## JSON #########################
-file = 'doc.json'
+
 def json_to_dict(file):
     f =open(file)
     return json.load(f)
-
-print(json_to_dict(file))
--- convertir.py
+++ convertir.py
@@ -4,7 +4,6 @@ import csv
 import xml.etree.ElementTree as T
 from csv import DictReader as csv_load
 from csv import DictWriter as csv_write
-from sqlalchemy import JSON
 import yaml
 from Conv import *
 from xmltodict import parse
@@ -14,7 +13,6 @@ from xmltodict import parse
 ###########################################################
 
 
-d = {'students': {'student': [{'name': 'Rick Grimes', 'rollnumber': '1', 'age': '15'}, {'name': 'Lori Grimes', 'rollnumber': '2', 'age': '16'}, {'name': 'Carl Grimes', 'rollnumber': '3', 'age': '14'}, {'name': 'Judith Grimes', 'rollnumber': '4', 'age': '13'}]}}
 def dict_to_json(dico):
     try:
         with open("moi.json", "w") as file:
@@ -28,28 +26,26 @@ def dict_to_json(dico):
 ##############################################################
 #                           YAML                             #
 ##############################################################
+
+
 def dict_to_yaml(dic):
-    with open("yama.yaml", "w") as wfile:
+    with open("b.yaml", "w",encoding='utf8') as wfile:
         dic = yaml.dump(dic, wfile)
-    print(dic)
-# dict_to_yaml(d)
-
-
-
+    print("Fichier crée avec succés")
 
 
 ##############################################################
 #                            CSV                             #
 ##############################################################
-def dict_to_csv(data):
-    with open('file.csv', 'w') as f:   
+def dict_to_csv(data, name):
+    with open(f'{name}.csv', 'w',encoding='utf8') as f:
         data = [data]
         fieldnames = data[0].keys()
         output = csv_write(f, fieldnames)
         output.writeheader()
         for elem in data:
             output.writerow(elem)
-
+        print("Fichier crée avec succés")
 
 #############################################################
 #                             XML                           #
